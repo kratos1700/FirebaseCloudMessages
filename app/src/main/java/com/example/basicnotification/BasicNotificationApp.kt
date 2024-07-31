501a4553fed952649d947d844adca2e19a60d8da
kratos1700 <hunterstarsa@hotmail.com>
2024-07-31 15:18:23 +0200

    Commit inicial, preparacion proyecto con Kotlin 2.0 , DI y Firebase Cloud Messaging

---
+++ app/src/main/java/com/example/basicnotification/BasicNotificationApp.kt
@@ -0,0 +1,8 @@
+package com.example.basicnotification
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BasicNotificationApp : Application() {
+}
